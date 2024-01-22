81380e3dd6b740eee4a79225bd6b17da587f4196
Ali Azhari <aliazhari@gmail.com>
2024-01-22 10:34:54 -0500

    first commit For a tutorial

---
+++ Azhari_A_Project1.java
@@ -0,0 +1,15 @@
+
+/**
+ * @Author: Ali Azhari
+ * @Filename: Azhari_A_Project1.java
+ * @Date: 01/22/2024
+ * @Description: This program is a tutorial blahha
+ *               bbkdhjkshd
+ * 
+ */
+
+//  a line Comments
+
+public class Azhari_A_Project1 {
+
+}
